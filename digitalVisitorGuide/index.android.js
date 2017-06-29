1550db8f3facb540f80440092d1a46e7c73ec049
Andy Wooller <andy.wooller@weclick.media>
2017-06-29 11:45:39 +0100

    welcome screen

--- digitalVisitorGuide/index.android.js
+++ digitalVisitorGuide/index.android.js
@@ -6,48 +6,86 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button
 } from 'react-native';
 
 export default class digitalVisitorGuide extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native test!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Image style={styles.image} source={require('./welcome_screen.jpg')}>
+                <View style={styles.backdropView}>
+                    <Image style={styles.logo} source={require('./hcp_icon.png')} />
+                    <Text style={styles.logoText}>Hampton Court Palace</Text>
+                    <Text style={styles.textTitle}>A Journey of Discovery</Text>
+                    <Button style={styles.button} title="Show me where to start"/>
+                </View>
+            </Image>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    imageContainer: {
+        flex: 1,
+        width: null,
+        height: null
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        width: null,
+        height: null,
+    },
+    backdropView: {
+        width: null,
+        height: null,
+        backgroundColor: 'rgba(10,0,0,0)'
+    },
+    textTitle: {
+
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 35,
+        paddingLeft: 25,
+        paddingRight: 25
+    },
+    logo: {
+        height: 50,
+        resizeMode: 'contain',
+        width: null,
+        marginTop: 115
+    },
+    logoText:{
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'Farao OT',
+        marginBottom: 50
+    },
+    button:{
+        color: 'black'
+    }
 });
 
 AppRegistry.registerComponent('digitalVisitorGuide', () => digitalVisitorGuide);
